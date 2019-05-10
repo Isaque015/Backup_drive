87874389ffc3378e0dd43060898b32db381ce656
isaque015 <isquefer@tutanota.com>
2019-05-10 17:27:27 -0300

    change drive to mega

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from setup_log import logger
 
 path_home = os.environ['HOME']
 path_dirs_local = '/Documents/livros/'
-path_dirs_drive = '/gdrive/Livros/'
-path_gdrive = path_home + '/gdrive'
+path_dirs_mega = '/megasync_up/livros/'
+path_mega = path_home + '/megasync_up'
 
-main_path_drive = path_home + path_dirs_drive
+main_path_mega = path_home + path_dirs_mega
 main_path = path_home + path_dirs_local
 
 dirs_not_empty = [
@@ -17,18 +17,12 @@ dirs_not_empty = [
 ]
 
 
-def check_mounted_gdrive(path_gdrive):
+def check_mounted_mega(path_mega):
 
-    dirs_drive = [
-        dirs for dirs in os.listdir(path_gdrive) if not dirs.startswith('.')
-    ]
-
-    if not dirs_drive:
-        logger.error('gdrive was not mounted or folder was excluded')
-        return dirs_drive
-
-    dirs_drive = True
-    return dirs_drive
+    if not os.path.exists(path_mega):
+        logger.error('mega was not mounted or folder was excluded')
+        return False
+    return True
 
 
 def check_dirs_empty(dirs_not_empty, main_path):
@@ -43,58 +37,62 @@ def check_dirs_empty(dirs_not_empty, main_path):
     return True
 
 
-def create_dirs_drive(main_path_drive, dirs_local_list, dirs_drive):
+def create_dirs_mega(main_path_mega, dirs_local_list, dirs_mega):
 
-    if not dirs_drive:
-        logger.error('gdrive was not mounted or folder was excluded')
+    if not dirs_mega:
+        logger.error('mega was not mounted or folder was excluded')
         return False
 
-    if not os.path.isdir(main_path_drive):
-        os.mkdir(main_path_drive)
-        logger.info(f'The folder {main_path_drive} was created.')
+    if not os.path.isdir(main_path_mega):
+        os.mkdir(main_path_mega)
+        logger.info(f'The folder {main_path_mega} was created.')
 
     logger_list_mv_folders = []
     for dirs in dirs_local_list:
 
-        if os.path.exists(main_path_drive + '/' + dirs):
+        if os.path.exists(main_path_mega + '/' + dirs):
             continue
 
-        os.mkdir(main_path_drive + '/' + dirs)
+        os.mkdir(main_path_mega + '/' + dirs)
         logger_list_mv_folders.append(dirs)
 
-    logger.info(
-        f'the folders were created {logger_list_mv_folders} \
-        \non the path {main_path_drive}.'
-    )
+    if logger_list_mv_folders:
+        logger.info(
+            f'the folders were created {logger_list_mv_folders} \
+            \non the path {main_path_mega}.'
+        )
+
     return True
 
 
-def copy_files_drive(main_path_drive, dirs_not_empty, main_path):
+def copy_files_mega(main_path_mega, dirs_not_empty, main_path):
 
     log_list_mv_files = []
     for dirs in dirs_not_empty:
         files_list = next(os.walk(main_path + dirs))[2]
-        files_list_drive = next(os.walk(main_path_drive + dirs))[2]
+        files_list_mega = os.listdir(main_path_mega + dirs)
 
         for files in files_list:
 
-            if files in files_list_drive:
+            if files in files_list_mega:
                 continue
 
             copyfile(
                 main_path + dirs + '/' + files,
-                main_path_drive + dirs + '/' + files
+                main_path_mega + dirs + '/' + files
             )
 
-            log_list_mv_files.append(main_path_drive + dirs + '/' + files)
-    logger.info(f'the files were created {log_list_mv_files}')
+            log_list_mv_files.append(main_path_mega + dirs + '/' + files)
+
+    if log_list_mv_files:
+        logger.info(f'the files were created {log_list_mv_files}')
     return True
 
 
-dirs_drive = check_mounted_gdrive(path_gdrive)
+dirs_mega = check_mounted_mega(path_mega)
 check = check_dirs_empty(dirs_not_empty, main_path)
 
-if check and dirs_drive:
-    result = create_dirs_drive(main_path_drive, dirs_not_empty, dirs_drive)
+if check and dirs_mega:
+    result = create_dirs_mega(main_path_mega, dirs_not_empty, dirs_mega)
     if result:
-        copy_files_drive(main_path_drive, dirs_not_empty, main_path)
+        copy_files_mega(main_path_mega, dirs_not_empty, main_path)
